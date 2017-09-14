ade5b9d6394d8570190e340efa66da937d3bfba5
wz2cool <wz2cool@live.cn>
2017-09-14 18:17:16 +0800

    add

--- 1.base-learning/src/client/index.tsx
+++ 1.base-learning/src/client/index.tsx
@@ -1,36 +1,32 @@
+// import * as React from "react";
+// import * as ReactDOM from "react-dom";
+// import * as ReactRouter from "react-router";
+// import { AppContainer } from "react-hot-loader";
+// import "./index.scss";
+
+
+// declare var module: {
+//     hot: {
+//         accept: (path: string, change: () => void) => void,
+//     },
+// };
+
+// ReactDOM.render(
+//     <AppContainer>
+//         <AppRouter />
+//     </AppContainer>,
+//     document.getElementById("example"));
+
+// if (module.hot) {
+//     module.hot.accept("./containers/AppRouter", () => {
+//         const NextAppRouter = require("./containers/AppRouter").default;
+//         ReactDOM.render(
+//             <AppContainer>
+//                 <NextAppRouter />
+//             </AppContainer>,
+//             document.getElementById("example"));
+//     });
+// }
 import * as React from "react";
-import * as ReactDOM from "react-dom";
-import * as ReactRouter from "react-router";
-import * as Models from "./models";
-import AppRouter from "./containers/AppRouter";
-import { AppContainer } from "react-hot-loader";
-import "./index.scss";
-
-declare var require: {
-    (path: string): Models.RequireRoute;
-    (paths: string[], callback: (...modules: any[]) => void): void;
-    ensure: (paths: string[], callback: (require: <T>(path: string) => T) => void, routeName: string) => void;
-};
-
-declare var module: {
-    hot: {
-        accept: (path: string, change: () => void) => void,
-    },
-};
-
-ReactDOM.render(
-    <AppContainer>
-        <AppRouter />
-    </AppContainer>,
-    document.getElementById("example"));
-
-if (module.hot) {
-    module.hot.accept("./containers/AppRouter", () => {
-        const NextAppRouter = require("./containers/AppRouter").default;
-        ReactDOM.render(
-            <AppContainer>
-                <NextAppRouter />
-            </AppContainer>,
-            document.getElementById("example"));
-    });
-}
+import { render } from "react-dom";
+import { createStore } from "redux";
--- 1.base-learning/src/client/routes/Main/Housework/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-
-class Housework extends React.Component<any, any> {
-    public render(): JSX.Element {
-        return (<span>11qqq1112222221222222221</span>);
-    }
-}
-
-export default Housework;
